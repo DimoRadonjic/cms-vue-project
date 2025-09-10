b9a8a56999f3dfd790483903e8171aa56efecc3a
Dimitrije Radonjić <dimo.radonjic2112@gmail.com>
2025-09-10 21:56:25 +0200

    removed unused for deployment

--- src/components/forms/FormEditPost.vue
+++ src/components/forms/FormEditPost.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import type { FormSubmitEvent } from "@primevue/forms";
 import { zodResolver } from "@primevue/forms/resolvers/zod";
-import { computed, reactive, ref, watch, watchEffect } from "vue";
+import { computed, reactive, ref, watchEffect } from "vue";
 import { useToastService } from "../../composable/toastService/AppToastService";
 import apiPosts from "../../axios/api/posts";
 import type {
